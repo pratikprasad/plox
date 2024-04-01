1f14a2bb65aefc8fb6e90678a449e9de1fa6f878
p <ratapratik@pp.home>
2024-04-01 09:50:34 -1000

    Autoformat

--- lox.py
+++ lox.py
@@ -1,21 +1,26 @@
 import sys
 
+
 def err(line, message):
     print(f"[line {line}] Error where: {message}")
 
+
 def run(content):
     print(content)
+
 
 def runFile(filename):
     with open(filename) as contents:
         data = contents.read()
         run(data)
 
+
 def runPrompt():
     while True:
         line = input("> ")
         run(line)
     print("Repl goes here")
+
 
 args = sys.argv[1:]
 if len(args) > 1:
@@ -26,4 +31,3 @@ if len(args) == 1:
     runFile(args[0])
 elif len(args) < 1:
     runPrompt()
-
