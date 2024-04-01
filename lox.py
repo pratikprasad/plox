b8b34e86a59f3439af747af6202b9f146fdcc848
p <ratapratik@pp.home>
2024-04-01 09:45:13 -1000

    Add scaffolding

--- lox.py
+++ lox.py
@@ -1,1 +1,29 @@
-print("hello")
+import sys
+
+def err(line, message):
+    print(f"[line {line}] Error where: {message}")
+
+def run(content):
+    print(content)
+
+def runFile(filename):
+    with open(filename) as contents:
+        data = contents.read()
+        run(data)
+
+def runPrompt():
+    while True:
+        line = input("> ")
+        run(line)
+    print("Repl goes here")
+
+args = sys.argv[1:]
+if len(args) > 1:
+    print("Usage: plox [script]")
+    exit(64)
+
+if len(args) == 1:
+    runFile(args[0])
+elif len(args) < 1:
+    runPrompt()
+
